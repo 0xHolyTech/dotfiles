41304a0375826e9423089d128d99e3e80ffc87af
Holy Tech <holytech.twitch@gmail.com>
2025-03-20 17:47:19 -0600

    Add nvim bindings (missing some plugins)

--- dot_config/nvim/lua/bindings/nvim-bindings.lua
+++ dot_config/nvim/lua/bindings/nvim-bindings.lua
@@ -1,2 +1,63 @@
 vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.conceallevel = 2
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes:2"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.g.mapleader = " "
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- vim.keymap.set("n", "J", "mzJ`z") Join bottom line
+vim.keymap.set("n", "<C-j>", "<C-d>zz")
+vim.keymap.set("n", "<C-k>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set('n', '<leader>pc', '<cmd>silent !tmux-closer<CR>')
+vim.keymap.set('n', '<leader>po', '<cmd>silent !byobu neww tmux-sessionizer<CR>')
+
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz") Trouble config
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz") Trouble config
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz") Trouble config
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz") Trouble config
+
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
