c6326c9fab25d22831976a777a1fa0fe599fc5f0
Holy Tech <holytech.twitch@gmail.com>
2025-04-18 13:12:34 -0600

    Fix telescope

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,8 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>sF', builtin.find_files, {})
+vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>sf', function()
+vim.keymap.set('n', '<leader>sF', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+    -- make sure ripgrep is installed
 end)
 
