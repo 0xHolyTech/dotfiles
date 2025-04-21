f226983ef73d94cd2186cd194ac3a9ace196f649
Holy Tech <holytech.twitch@gmail.com>
2025-04-21 12:03:31 -0600

    Set key function for harpoon git folders

--- dot_config/nvim/after/plugin/harpoon.lua
+++ dot_config/nvim/after/plugin/harpoon.lua
@@ -8,10 +8,21 @@ harpoon:setup({
                 return string.gsub(root, "\n", "")
             end
             return cwd
+        end
+        },
+    settings = {
+        save_on_toggle = false,
+        sync_on_ui_close = false,
+        key = function()
+            local cwd = vim.loop.cwd()
+            local root = vim.fn.system("git rev-parse --show-toplevel")
+            if vim.v.shell_error == 0 and root ~= nil then
+                return root
             end
-        }
-    }
-)
+            return cwd
+        end,
+    },
+})
 
 
 vim.keymap.set("n", "<leader>ea", function() harpoon:list():add() end)
