d6e2a1c9310a27527204d0e699c607bff86967e0
Holy Tech <holytech.twitch@gmail.com>
2025-05-02 18:15:05 -0600

    Fix script

--- dot_config/private_fish/functions/windows_connect.fish
+++ dot_config/private_fish/functions/windows_connect.fish
@@ -1,3 +1,3 @@
-function windows_connect --wraps=xfreerdp /v:192.168.122.95 /u:HolyVM /p:123 /size:100% /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive & 'alias windows_connect=xfreerdp /v:192.168.122.95 /u:HolyVM /p:123 /size:100% /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive &'
+function windows_connect --wraps='xfreerdp /v:192.168.122.95 /u:HolyVM /p:123 /size:100% /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive &' 'alias windows_connect=xfreerdp /v:192.168.122.95 /u:HolyVM /p:123 /size:100% /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive &'
     xfreerdp /v:192.168.122.95 /u:HolyVM /p:123 /size:100% /d: /dynamic-resolution /gfx-h264:avc444 +gfx-progressive &
 end
