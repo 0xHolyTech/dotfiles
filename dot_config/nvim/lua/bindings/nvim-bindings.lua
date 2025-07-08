1b23a251a3c8083d47c8db6295a87b038017fd26
Holy Tech <holytech.twitch@gmail.com>
2025-07-08 16:19:55 -0600

    Add replace from visual mode

--- dot_config/nvim/lua/bindings/nvim-bindings.lua
+++ dot_config/nvim/lua/bindings/nvim-bindings.lua
@@ -69,5 +69,6 @@ vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left>
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+vim.keymap.set("v", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
 
