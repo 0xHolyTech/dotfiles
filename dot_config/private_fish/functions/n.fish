f5c792c7546c8f5a88ee678360fe36ec1f44c3ec
Holy Tech <holytech.twitch@gmail.com>
2025-07-09 23:21:48 -0600

    Remove echos

--- dot_config/private_fish/functions/n.fish
+++ dot_config/private_fish/functions/n.fish
@@ -1,4 +1,4 @@
 function n --description 'alias nvim=n --listen /tmp/nvim.pipe'
-    export NVIM_SOCKET=$(mktemp -u -t nvim_XXXX.pipe) && echo $NVIM_SOCKET && nvim --listen $NVIM_SOCKET $argv
+    export NVIM_SOCKET=$(mktemp -u -t nvim_XXXX.pipe) && nvim --listen $NVIM_SOCKET $argv
 end
 
--- dot_config/private_fish/functions/vim.fish
+++ dot_config/private_fish/functions/vim.fish
@@ -1,4 +1,4 @@
 function vim --wraps=nvim --description 'alias vim=nvim --listen /tmp/nvim.pipe'
-    export NVIM_SOCKET=$(mktemp -u -t nvim_XXXX.pipe) && echo $NVIM_SOCKET && nvim --listen $NVIM_SOCKET $argv
+    export NVIM_SOCKET=$(mktemp -u -t nvim_XXXX.pipe) && nvim --listen $NVIM_SOCKET $argv
 end
 
