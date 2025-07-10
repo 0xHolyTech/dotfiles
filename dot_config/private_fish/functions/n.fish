298a997a0e9175843a353fe77049dc7c084631d5
Holy Tech <holytech.twitch@gmail.com>
2025-07-09 23:20:48 -0600

    Allow randomized socket on creation of nvim instance

--- dot_config/private_fish/functions/n.fish
+++ dot_config/private_fish/functions/n.fish
@@ -1,4 +1,4 @@
-function n --description 'alias nvim=n'
-    nvim $argv
+function n --description 'alias nvim=n --listen /tmp/nvim.pipe'
+    export NVIM_SOCKET=$(mktemp -u -t nvim_XXXX.pipe) && echo $NVIM_SOCKET && nvim --listen $NVIM_SOCKET $argv
 end
 
