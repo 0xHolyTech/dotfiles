a434fd106431d855bad3aa06182904e5b1261a19
Holy Tech <holytech.twitch@gmail.com>
2025-10-25 17:39:14 -0600

    Shortcuts for code companion

--- dot_config/nvim/lua/config/codecompanion.lua
+++ dot_config/nvim/lua/config/codecompanion.lua
@@ -3,3 +3,8 @@ require("codecompanion").setup({
     log_level = "DEBUG", -- or "TRACE"
   }
 })
+
+vim.keymap.set("n", "<leader>cp", "<cmd>CodeCompanion<CR>", {})
+vim.keymap.set("n", "<leader>ca", "<cmd>CodeCompanionActions<CR>", {})
+vim.keymap.set("n", "<leader>cc", "<cmd>CodeCompanionChat<CR>", {})
+vim.keymap.set("n", "<leader>cC", "<cmd>CodeCompanionCmd<CR>", {})
