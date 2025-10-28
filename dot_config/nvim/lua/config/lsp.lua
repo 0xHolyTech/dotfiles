32940d68d70b4678b984b3b99187bafe7045f53b
Holy Tech <holytech.twitch@gmail.com>
2025-10-28 17:01:20 -0600

    Add astro

--- dot_config/nvim/lua/config/lsp.lua
+++ dot_config/nvim/lua/config/lsp.lua
@@ -17,7 +17,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end,
 })
 
-vim.lsp.enable({ 'basedpyright', 'lua_ls', 'gopls', 'gdscript', 'waspls' })
+vim.lsp.enable({ 'astro', 'basedpyright', 'lua_ls', 'gdscript', 'gopls', 'waspls' })
 
 require("luasnip.loaders.from_vscode").lazy_load()
 
