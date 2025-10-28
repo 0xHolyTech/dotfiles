dce848b8420478585aa323ebb04ad413a6bda229
Holy Tech <holytech.twitch@gmail.com>
2025-10-28 15:01:04 -0600

    Add treesitter for wasp

--- dot_config/nvim/lua/config/treesitter.lua
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -26,6 +26,19 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.wasp = {
+  install_info = {
+    url = "https://github.com/bx2/tree-sitter-wasp", -- local path or git repo
+    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+  filetype = "wasp", -- if filetype does not match the parser name
+}
+
 vim.keymap.set('n', '<leader>sj', '<cmd>TSJJoin<CR>')
 vim.keymap.set('n', '<leader>ss', '<cmd>TSJSplit<CR>')
 vim.keymap.set("v", "<TAB>", ">gv")
