3e95c39268ba81ff288881f0f7e4af7542b26d47
Holy Tech <holytech.twitch@gmail.com>
2025-10-28 20:47:59 -0600

    Remove wasp from running on non wasp files

--- dot_config/nvim/lsp/waspls.lua
+++ dot_config/nvim/lsp/waspls.lua
@@ -1,5 +1,5 @@
 return {
     cmd = { 'wasp', 'waspls', '--stdio' },
     root_markers = { 'app/.wasproot', '.wasproot' },
-    filetypes = {'wasp', 'typescript', 'typescriptreact', 'javascript' },
+    filetypes = { 'wasp' },
 }
