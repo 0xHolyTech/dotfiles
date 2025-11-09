1be8b4b814317d98a52f089e7b7aee7fff1133d5
Holy Tech <holytech.twitch@gmail.com>
2025-11-09 16:00:35 -0600

    Remove unneeded decyrpt command args

--- dot_config/private_fish/functions/sops_decrypt.fish
+++ dot_config/private_fish/functions/sops_decrypt.fish
@@ -1,4 +1,4 @@
 function sops_decrypt --wraps='sops --decrypt --age $(cat ~/.ssh/id_age | grep -oP "public key: \K(.*)") --encrypted-regex REGEX --in-place FILE_PATH' --description 'sops_decrypt $FILE_PATH $REGEX'
-    sops --decrypt --age $(cat ~/.ssh/id_age | grep -oP "AGE-SECRET-KEY(.*)") --encrypted-regex $argv[2] --in-place $argv[1]
+    sops --decrypt --in-place $argv[1]
 end
 
