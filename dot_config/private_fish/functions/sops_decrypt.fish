930bef382d440e37f4bfba532f4b95cfb9d28eb3
Holy Tech <holytech.twitch@gmail.com>
2025-11-09 15:29:23 -0600

    Fixes for commands SOPS

--- dot_config/private_fish/functions/sops_decrypt.fish
+++ dot_config/private_fish/functions/sops_decrypt.fish
@@ -1,4 +1,4 @@
-function sops_decrypt --wraps='sops --decrypt --age $(cat KEY_FILE_PATH | grep -oP "public key: \K(.*)") --encrypted-regex '^(data|stringData)$' --in-place FILE_PATH' --description 'sops_decrypt $AGE_KEY_FILE_PATH $FILE_PATH'
-    sops --decrypt --age $(cat $item[1] | grep -oP "public key: \K(.*)") --encrypted-regex '^(data|stringData)$' --in-place $item[2]
+function sops_decrypt --wraps='sops --decrypt --age $(cat ~/.ssh/id_age | grep -oP "public key: \K(.*)") --encrypted-regex REGEX --in-place FILE_PATH' --description 'sops_decrypt $FILE_PATH $REGEX'
+    sops --decrypt --age $(cat ~/.ssh/id_age | grep -oP "public key: \K(.*)") --encrypted-regex $argv[2] --in-place $argv[1]
 end
 
--- dot_config/private_fish/functions/sops_encrypt.fish
+++ dot_config/private_fish/functions/sops_encrypt.fish
@@ -1,4 +1,4 @@
-function sops_encrypt --wraps='sops --encrypt --age $(cat KEY_FILE_PATH | grep -oP "public key: \K(.*)") --encrypted-regex '^(data|stringData)$' --in-place FILE_PATH' --description 'sops_encrypt $AGE_KEY_FILE_PATH $FILE_PATH'
-    sops --encrypt --age $(cat $item[1] | grep -oP "public key: \K(.*)") --encrypted-regex '^(data|stringData)$' --in-place $item[2]
+function sops_encrypt --wraps='sops --encrypt --age $(cat ~/.ssh/id_age | grep -oP "public key: \K(.*)") --encrypted-regex REGEX --in-place FILE_PATH' --description 'sops_encrypt $FILE_PATH $REGEX'
+    sops --encrypt --age $(cat ~/.ssh/id_age | grep -oP "public key: \K(.*)") --encrypted-regex $argv[2] --in-place $argv[1]
 end
 
