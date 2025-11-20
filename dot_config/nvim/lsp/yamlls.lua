7bdf0259e847958c45e2595a9069e7cde4716bc9
Holy Tech <holytech.twitch@gmail.com>
2025-11-20 16:16:18 -0600

    Fix kustomization and add helmfile support

--- dot_config/nvim/lsp/yamlls.lua
+++ dot_config/nvim/lsp/yamlls.lua
@@ -2,8 +2,9 @@ return {
     settings = {
         yaml = {
             schemas = {
-                ["http://json.schemastore.org/kustomization"] = "{kustomization,kustomize}.{yml,yaml}",
-                ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+                ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+                ["https://www.schemastore.org/helmfile.json"] = "helmfile.{yml,yaml}",
+                ["https://gitlab.com/gitlab-org/gitlab-foss/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
             },
         }
     }
