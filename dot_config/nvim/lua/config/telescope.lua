f4be774393d42e5f971dcebeeeb567ee2d418958
Holy Tech <holytech.twitch@gmail.com>
2025-11-22 11:17:45 -0600

    Allow hidden files in telescope find files

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -1,5 +1,7 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>sf', function()
+    builtin.find_files({ hidden = true })
+end, {})
 vim.keymap.set('n', '<leader>sg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>sF', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
