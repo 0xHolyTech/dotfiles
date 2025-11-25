5dbd1924aaacc2e2a344af75794203e33fcbb06b
Holy Tech <holytech.twitch@gmail.com>
2025-11-25 12:35:42 -0600

    Adding config to helm-ls

--- dot_config/nvim/lsp/helm_ls.lua
+++ dot_config/nvim/lsp/helm_ls.lua
@@ -1,8 +1,17 @@
 return {
     settings = {
         ['helm-ls'] = {
+            valuesFiles = {
+                mainValuesFile = "values.yaml",
+                lintOverlayValuesFile = "values.lint.yaml",
+                additionalValuesFilesGlobPattern = "values*.yaml"
+            },
+            helmLint = {
+                enabled = true,
+                ignoredMessages = {},
+            },
             yamlls = {
-                path = "yaml-language-server",
+                path = { "node", "yaml-language-server.js" },
             }
         }
     }
