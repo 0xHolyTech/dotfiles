521f8d6c41b39a13e817fe8f0d7f504aa2632640
Holy Tech <holytech.twitch@gmail.com>
2025-11-25 11:21:26 -0600

    Helm treesitter

--- dot_config/nvim/lua/config/treesitter.lua
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "typescript", "python", "go" },
+  ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "typescript", "python", "go", "yaml", "helm" },
   sync_install = false,
   auto_install = true,
   ignore_install = { "javascript" },
