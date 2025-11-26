f41e5005a59499405e088764403f4f6446f22f88
Holy Tech <holytech.twitch@gmail.com>
2025-11-25 23:56:16 -0600

    Add kubernetes default file

--- dot_config/nvim/lsp/yamlls.lua
+++ dot_config/nvim/lsp/yamlls.lua
@@ -2,6 +2,7 @@ return {
     settings = {
         yaml = {
             schemas = {
+                kubernetes = "*.yaml",
                 ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
                 ["https://www.schemastore.org/helmfile.json"] = "helmfile.{yml,yaml}",
                 ["https://gitlab.com/gitlab-org/gitlab-foss/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
