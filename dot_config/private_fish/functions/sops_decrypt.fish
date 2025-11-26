cadf01743b58fdf4ffd5da2398305bcb4f166c1d
Holy Tech <holytech.twitch@gmail.com>
2025-11-25 18:39:00 -0600

    Ignore mac

--- dot_config/private_fish/functions/sops_decrypt.fish
+++ dot_config/private_fish/functions/sops_decrypt.fish
@@ -1,4 +1,4 @@
 function sops_decrypt --wraps='sops --decrypt --age $(cat ~/.ssh/id_age | grep -oP "public key: \K(.*)") --encrypted-regex REGEX --in-place FILE_PATH' --description 'sops_decrypt $FILE_PATH $REGEX'
-    sops --decrypt --in-place $argv[1]
+    sops --decrypt --in-place --ignore-mac $argv[1]
 end
 
