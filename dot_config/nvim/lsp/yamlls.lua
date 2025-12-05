773cd04a64b607b61bf1c451d352f60f6a7df82d
Holy Tech <holytech.twitch@gmail.com>
2025-12-05 15:11:31 -0600

    Remove docker compose from yamlls

--- dot_config/nvim/lsp/yamlls.lua
+++ dot_config/nvim/lsp/yamlls.lua
@@ -8,8 +8,11 @@ return {
     },
     settings = {
         yaml = {
+            schemaStore = {
+                enable = false,
+            },
             schemas = {
-                [require('kubernetes').yamlls_schema()] = "*.yaml",
+                [require('kubernetes').yamlls_schema()] = "[!docker*]*.{yaml,yml}",
                 ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
                 ["https://www.schemastore.org/helmfile.json"] = "helmfile.{yml,yaml}",
                 ["https://gitlab.com/gitlab-org/gitlab-foss/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
