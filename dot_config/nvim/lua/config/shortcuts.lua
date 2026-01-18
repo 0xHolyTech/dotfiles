4d69124f7878f4d40d95c13279d4cbbd16ecb5c9
Holy Tech <holytech.twitch@gmail.com>
2026-01-17 23:43:36 -0600

    Add shortcut template for me

--- dot_config/nvim/lua/config/shortcuts.lua
+++ dot_config/nvim/lua/config/shortcuts.lua
@@ -1,4 +1,64 @@
-require('shortcuts').setup()
+require('shortcuts').setup(
+    {
+        override = true,
+        defaults = {
+            empty = {},
+            default = {},
+            python = {
+                n = {
+                    l = {
+                      command_type = "async",
+                      async_type = "run",
+                      command = "uv run black ."
+                    }
+                }
+            },
+            docker = {
+                n = {
+                    b = {
+                      command_type = "async",
+                      async_type = "term",
+                      command = "docker compose build"
+                    },
+                    u = {
+                      command_type = "async",
+                      async_type = "run",
+                      command = "docker compose up -d",
+                      notify = true
+                    },
+                    d = {
+                      command_type = "async",
+                      async_type = "run",
+                      command = "docker compose down",
+                      notify = true
+                    },
+                },
+            },
+            golang = {
+                n = {
+                    u = {
+                        command_type = "async",
+                        async_type = "term",
+                        command = "go run main.go",
+                    },
+                    b = {
+                        command_type = "async",
+                        async_type = "term",
+                        command = "go build main.go",
+                    },
+                },
+            },
+            lua = {
+                n = {
+                    l = {
+                        command = "print('Example lua command')",
+                        command_type = "lua",
+                    }
+                }
+            }
+        },
+    }
+)
 
 vim.keymap.set('n', '<leader>aa', '<Cmd>ShortcutsToggle<CR>')
 
