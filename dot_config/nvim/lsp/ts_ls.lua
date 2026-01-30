ad07d9371ba55744626cb0b745c71f61cd799af6
Holy Tech <holytech.twitch@gmail.com>
2026-01-30 15:04:24 -0600

    Fix file type for ts needed for vue

--- dot_config/nvim/lsp/ts_ls.lua
+++ dot_config/nvim/lsp/ts_ls.lua
@@ -3,5 +3,5 @@ local util = require('lspconfig.util')
 return {
     cmd = { 'typescript-language-server', '--stdio' },
     root_dir = util.root_pattern('tsconfig.json', 'jsconfig.json', 'package.json', '.git'),
-    filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'typescript.jsx', 'javascript.jsx' },
+    filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'typescript.jsx', 'javascript.jsx', 'vue' },
 }
