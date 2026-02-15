bbf04d915b3e301538df1da37431d946e85fa143
Holy Tech <holytech.twitch@gmail.com>
2026-02-15 00:53:56 -0600

    Add lualine full path of file

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -79,7 +79,14 @@ require('lazy').setup({
         { 'mbbill/undotree' }, -- tu
         {
             'nvim-lualine/lualine.nvim',
-            dependencies = { 'nvim-tree/nvim-web-devicons' }
+            dependencies = { 'nvim-tree/nvim-web-devicons' },
+            config = function()
+                require('lualine').setup({
+                    sections = {
+                        lualine_c = { { 'filename', path = 1 } }
+                    },
+                })
+            end,
         },
         {
             'max397574/better-escape.nvim',
