c9dc4fb37129519556d12c8803d98278070196eb
Holy Tech <holytech.twitch@gmail.com>
2026-06-17 09:30:36 -0600

    Fix set env for spaces bewteen equal

--- dot_config/private_fish/functions/set_env.fish
+++ dot_config/private_fish/functions/set_env.fish
@@ -1,7 +1,9 @@
 function set_env
   for line in (cat $argv | grep -v '^#')
     set item (string split -m 1 '=' $line)
-    set -gx $item[1] $item[2]
+    set var1 (echo $item[1] | tr -d '[:space:]')
+    set var2 (echo $item[2] | tr -d '[:space:]')
+    set -gx $var1 $var2
     # echo "Exported key $item[1]"
   end
 end
