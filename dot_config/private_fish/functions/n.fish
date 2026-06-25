8b2576206db05da1b5da64151a41f3c9539910af
Holy Tech <holytech.twitch@gmail.com>
2026-06-25 16:23:57 -0600

    Add tuxedo script to nvim init

--- dot_config/private_fish/functions/n.fish
+++ dot_config/private_fish/functions/n.fish
@@ -1,4 +1,4 @@
 function n --description 'alias nvim=n --listen /tmp/nvim.pipe'
-    export NVIM_SOCKET=$(mktemp -u -t nvim_XXXX.pipe) && nvim --listen $NVIM_SOCKET $argv
+    export TODO_DIR=$(tuxedo-dir) && export NVIM_SOCKET=$(mktemp -u -t nvim_XXXX.pipe) && nvim --listen $NVIM_SOCKET $argv
 end
 
