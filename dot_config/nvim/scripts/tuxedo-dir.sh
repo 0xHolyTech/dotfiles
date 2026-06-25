0655dee6e797a0aafbdfbe65c3de02420da2aa04
Holy Tech <holytech.twitch@gmail.com>
2026-06-25 16:23:48 -0600

    fix tuxedo script

--- dot_config/nvim/scripts/tuxedo-dir.sh
+++ dot_config/nvim/scripts/tuxedo-dir.sh
@@ -1,13 +1,15 @@
+#!/bin/bash
 inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
 
 if [ "$inside_git_repo" ]; then
-    export CurrentDir=$(git rev-parse --show-toplevel)
+    CurrentDir=$(git rev-parse --show-toplevel)
 else
-    export CurrentDir=$(pwd)
+    CurrentDir=$(pwd)
 fi
 
-export TUXEDO_DIR="~/.local/share/nvim/tuxedo"
-export TUXEDO_FOLDER="${CurrentDir//\//\.}"
-export TODO_DIR="${TUXEDO_DIR}/${TUXEDO_FOLDER}/"
-mkdir -p TODO_DIR
-return TODO_DIR
+TUXEDO_DIR="~/.local/share/nvim/tuxedo"
+TUXEDO_FOLDER="${CurrentDir//\//\.}"
+TODO_DIR="${TUXEDO_DIR}/${TUXEDO_FOLDER}"
+
+mkdir -p $TODO_DIR
+echo "$TODO_DIR"
